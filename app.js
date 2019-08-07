9d6cde375ebf9f2d1c344ca2ec2a51b6e8372d8c
devbrunopaula <brunopaula@dryserv.com>
2019-08-06 22:53:47 -0400

    working

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-
+const mysql = require("mysql");
 const logger = require('./middleware/Logger')
 const db = require("./src/db");
-
+const dotenv = require("dotenv");
 // Middleware
 app.use(logger);
 
+// Setting up the .env config
+dotenv.config({
+    path: './.env'
+})
+
+
 //DB Connect
+
+
+
 db.connect((err) =>{
     if(err){
        console.log(err)
@@ -15,8 +24,6 @@ db.connect((err) =>{
     console.log('SQL Connected')
     
 });
-
-// Setting up the .env config
 
 
 
--- src/db.js
+++ src/db.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './.env'
+})
+
 
 const db = mysql.createConnection({
-    host: '107.180.117.116',
-    user: 'dryservapi',
-    password: 'C@pecod508',
-    database: 'dryserv-api',
-    path: '/home/dryservcleaning',
-    port: '3306'
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.mysqlpassword,
+    database: process.env.database,
+    path: process.env.path,
+    port: process.env.msqlport 
 
 
 });
