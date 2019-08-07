e58e0723748559ca8c79959948f2120cb8943b78
devbrunopaula <brunopaula@dryserv.com>
2019-08-07 15:32:37 -0400

    add .ev

--- src/db.js
+++ src/db.js
@@ -6,12 +6,19 @@ dotenv.config({
 })
 
 
+// const db = mysql.createConnection({
+//    host: 'us-cdbr-iron-east-02.cleardb.net',
+//    user: 'b29eb30123d588',
+//    password: '142e8cec',
+//    database: 'heroku_68b6cba97538592'
+//    //`mysql://b29eb30123d588:142e8cec@us-cdbr-iron-east-02.cleardb.net/heroku_68b6cba97538592?reconnect=true`
+// });
 const db = mysql.createConnection({
-   host: 'us-cdbr-iron-east-02.cleardb.net',
-   user: 'b29eb30123d588',
-   password: '142e8cec',
-   database: 'heroku_68b6cba97538592'
-   //`mysql://b29eb30123d588:142e8cec@us-cdbr-iron-east-02.cleardb.net/heroku_68b6cba97538592?reconnect=true`
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.mysqlpassword,
+    database: process.env.database
+    //`mysql://b29eb30123d588:142e8cec@us-cdbr-iron-east-02.cleardb.net/heroku_68b6cba97538592?reconnect=true`
 });
 
 module.exports = db;
